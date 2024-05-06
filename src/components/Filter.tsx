4d2263f9c9d8c058bba324978456141d87fdff56
Diego Santos <diegofmsantos0@gmail.com>
2024-05-06 19:13:50 -0300

    ajustes css dos filtros

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -5,11 +5,13 @@ export const Filter = () => {
     return (
         <div className="flex justify-around my-6">
             <Select>
-                <SelectTrigger className="w-[180px] text-[#16C6D1] font-bold">
-                    <SelectValue placeholder="Selecione o ano" />
+                <SelectTrigger className="w-[180px] font-bold flex flex-col h-16 rounded-3xl">
+                    <span className="text-[#16C6D1]">Filtre por Ano</span>
+                    <SelectValue placeholder="2024" className="text-black" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectGroup>
+                    <SelectGroup className="font-bold">
+                        <SelectLabel>2024</SelectLabel>
                         <SelectItem value="2020">2020</SelectItem>
                         <SelectItem value="2021">2021</SelectItem>
                         <SelectItem value="2022">2022</SelectItem>
@@ -19,11 +21,12 @@ export const Filter = () => {
                 </SelectContent>
             </Select>
             <Select>
-                <SelectTrigger className="w-[180px]  text-[#16C6D1] font-bold">
-                    <SelectValue placeholder="Selecione o mês" />
+                <SelectTrigger className="w-[180px] font-bold flex flex-col h-16 rounded-3xl">
+                <span className="text-[#16C6D1]">Filtre por Mês</span>
+                    <SelectValue placeholder="Maio" className="text-black" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectGroup>
+                    <SelectGroup className="font-bold">
                         <SelectItem value="janeiro">Janeiro</SelectItem>
                         <SelectItem value="fevereiro">Fevereiro</SelectItem>
                         <SelectItem value="marco">Março</SelectItem>
